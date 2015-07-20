08599e201a697312013dcfba919e74b46599ee51
Cory Spitzer & david abel <student@epicodus.com>
2015-07-20 13:53:03 -0700

    Add error catching to message saving

--- spec/features/display_message_form_spec.rb
+++ spec/features/display_message_form_spec.rb
@@ -5,7 +5,7 @@ describe 'the new message form' do
     visit new_message_path
     fill_in 'To', with: '5033136555'
     fill_in 'From', with: '5039465235'
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content "From"
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,4 +4,9 @@ describe Message do
   # it { should validate_presence_of :to}
   # it { should validate_presence_of :from}
   # it { should validate_presence_of :body}
+
+  it "doesn't save the message if twilio gives an error" do
+    message = Message.new(:body => 'hi', :to => '1111111', :from => '5039463641')
+    expect(message.save).to be false
+  end
 end
